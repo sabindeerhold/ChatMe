ca07200662c1c27a5107bea0be7a0dc698cfe096
B22stsbnsim@ <ssimkhada@deerhold.org>
2022-11-25 10:09:10 +0545

    Initial Commit

---
+++ ChatMe/ChatMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatMeApp.swift
+//  ChatMe
+//
+//  Created by Sabin Simkhada on 25/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
